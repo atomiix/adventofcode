4ee8b2d2d86cedf4872c7f7739c19a33590ff539
Thomas Baccelli <thomasbaccelli@tabesto.com>
2023-12-05 12:19:37 +0100

    day4

--- 2023/day4/index.php
+++ 2023/day4/index.php
@@ -1,5 +1,28 @@
 <?php
 
-$lines = file(sprintf('%s/%s', __DIR__, isset($argv[2]) ? 'test.txt' : 'input.txt'));
+$lines = file('input.txt');
 
-var_dump($lines);
+$total = 0;
+$cards = [];
+foreach ($lines as $k => $card) {
+    if (!isset($cards[$k])) {
+        $cards[$k] = 0;
+    }
+    $cards[$k]++;
+    [$nn, $numbers] = explode(': ', trim($card));
+    [$list, $mine] = explode(' | ', $numbers);
+    $count = count(array_filter(array_intersect(explode(' ', $list), explode(' ', $mine))));
+    $pow = (int)(pow(2, $count)/2);
+    if ($pow > 0) {
+        for ($i = 1; $i <= $count; $i++) {
+            if (!isset($cards[$k+$i])) {
+                $cards[$k+$i] = 0;
+            }
+            $cards[$k+$i] += $cards[$k];
+        }
+    }
+    $total += $pow;
+}
+
+echo $total . PHP_EOL;
+echo array_sum($cards) . PHP_EOL;
