13a9052f12bcdcd569669f36a378fe2efb1979fb
atomiix <tbaccelli@gmail.com>
2024-12-01 23:18:06 +0100

    2024 day1

--- 2024/day1/index.php
+++ 2024/day1/index.php
@@ -1,1 +1,23 @@
 <?php
+
+$lines = file('input.txt', FILE_IGNORE_NEW_LINES);
+
+$left = [];
+$right = [];
+
+foreach ($lines as $line) {
+    [$left[], $right[]] = explode('   ', $line);
+}
+
+sort($left);
+sort($right);
+
+$diff = 0;
+$total = 0;
+$count = array_count_values($right);
+foreach ($left as $i => $v) {
+    $diff += abs($v - $right[$i]);
+    $total += ($count[$v] ?? 0) * $v;
+}
+
+echo "$diff | $total\n";
